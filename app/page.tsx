eef91d93fb7f2141dc2c577d9372db5fafb893dd
Karingi <emmanuelkaringi@gmail.com>
2026-02-02 14:13:19 +0300

    implement hompage companions cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to SautiEdu</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="The Brainy Explorer"
+          topic="Network of the brain"
+          subject="Math"
+          duration={50}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the brain"
+          subject="Geography"
+          duration={45}
+          color="#ffda6e"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
